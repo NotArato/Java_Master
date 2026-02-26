5b852c81e72afa36cb6ed864ee05f733eb323c08
[NotArato] <[pheng.an2016@gmail.com]>
2026-02-26 10:47:24 +0700

    1.2.1c

--- InheritanceExample/src/main/java/edu/handong/csee/java/example/inheritance/Undergraduate.java
+++ InheritanceExample/src/main/java/edu/handong/csee/java/example/inheritance/Undergraduate.java
@@ -1,49 +1,57 @@
 package edu.handong.csee.java.example.inheritance;
 
 /*
- * This class represents a Student, which is a subclass of Person. 
- * It has an additional attribute studentNumber and methods to get and set the student number, as well as a method to write the output of the student's information. 
- * It also has a method to compare two Student objects for equality based on their name and student number.
+	 * Undergraduate.java
+ * This class represents an undergraduate student, which is a subclass of the Student class. It has an additional property called level, which indicates the year of the undergraduate student (1 for freshman, 2 for sophomore, 3 for junior, or 4 for senior). The class also has methods to get and set the level, as well as a method to write the output of the undergraduate student's information and a method to compare two undergraduate students for equality.
+
 */
 
-public class Undergraduate extends Student {	// Undergraduate class is a subclass of Student class, which means it inherits all the properties and methods of Student class and can also have its own properties and methods.
-	private int level; 	//1 for freshman, 2 for sophomore 
-						//3 for junior, or 4 for senior. 
+public class Undergraduate extends Student { // Undergraduate class is a subclass of Student class, which means it
+												// inherits all the properties and methods of Student class and can also
+												// have its own properties and methods.
+	private int level; // 1 for freshman, 2 for sophomore
+						// 3 for junior, or 4 for senior.
 
-	public Undergraduate()    {	// default constructor
-		super();        
-		level = 1;  
-	} 
-
-	public Undergraduate(String initialName, int initialStudentNumber, int initialLevel) { // constructor with parameters
-		super(initialName, initialStudentNumber);        
-		setLevel(initialLevel); //checks 1 <= initialLevel <= 4    
+	public Undergraduate() { // default constructor
+		super();
+		level = 1;
 	}
 
-	public void reset(String newName, int newStudentNumber, int newLevel) { // method to reset the name, student number, and level of the undergraduate student
-		reset(newName, newStudentNumber); //Student's reset        
-		setLevel(newLevel); //Checks 1 <= newLevel <= 4    
+	public Undergraduate(String initialName, int initialStudentNumber, int initialLevel) { // constructor with
+																							// parameters
+		super(initialName, initialStudentNumber);
+		setLevel(initialLevel); // checks 1 <= initialLevel <= 4
 	}
 
-	public int getLevel() {	// method to get the level of the undergraduate student
-		return level;    
-	} 
+	public void reset(String newName, int newStudentNumber, int newLevel) { // method to reset the name, student number,
+																			// and level of the undergraduate student
+		reset(newName, newStudentNumber); // Student's reset
+		setLevel(newLevel); // Checks 1 <= newLevel <= 4
+	}
 
-	public void setLevel(int newLevel)    {	// method to set the level of the undergraduate student
-		if ((1 <= newLevel) && (newLevel <= 4))           
+	public int getLevel() { // method to get the level of the undergraduate student
+		return level;
+	}
+
+	public void setLevel(int newLevel) { // method to set the level of the undergraduate student
+		if ((1 <= newLevel) && (newLevel <= 4))
 			level = newLevel;
 		else {
-			System.out.println("Illegal level!");	// if the new level is not between 1 and 4, print an error message and exit the program
-			System.exit(0);       
-		}    
-	} 
+			System.out.println("Illegal level!"); // if the new level is not between 1 and 4, print an error message and
+													// exit the program
+			System.exit(0);
+		}
+	}
 
-	public void writeOutput()    { // method to write the name, student number, and level of the undergraduate student
-		super.writeOutput();        
-		System.out.println("StudentLevel: " + level);    
-	} 
+	public void writeOutput() { // method to write the name, student number, and level of the undergraduate
+								// student
+		super.writeOutput();
+		System.out.println("StudentLevel: " + level);
+	}
 
-	public boolean equals(Undergraduate otherUndergraduate) { // method to compare the name, student number, and level of this undergraduate student with another undergraduate student
-		return equals((Student)otherUndergraduate) && (this.level == otherUndergraduate.level);    
-	} 
+	public boolean equals(Undergraduate otherUndergraduate) { // method to compare the name, student number, and level
+																// of this undergraduate student with another
+																// undergraduate student
+		return equals((Student) otherUndergraduate) && (this.level == otherUndergraduate.level);
+	}
 }
